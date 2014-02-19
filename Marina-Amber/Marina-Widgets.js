c01894f83731d63208398cf8e012bb651270afb2
Nicolas Petton <petton.nicolas@gmail.com>
2014-02-19 17:27:21 +0100

    renames the project to Marina, fixes #1

---
+++ Marina-Amber/Marina-Widgets.js
@@ -0,0 +1,7 @@
+define("marina/Marina-Widgets", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_vm/globals", "amber_core/Web"], function(smalltalk,nil,_st, globals){
+smalltalk.addPackage('Marina-Widgets');
+smalltalk.packages["Marina-Widgets"].transport = {"type":"amd","amdNamespace":"marina"};
+
+smalltalk.addClass('MRLoginWidget', globals.Widget, [], 'Marina-Widgets');
+
+});
